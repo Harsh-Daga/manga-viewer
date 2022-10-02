6a62532f41d535a91d65fc08cef7bf3700b4988c
Harsh <64948227+Harsh-Daga@users.noreply.github.com>
2022-10-02 15:52:06 +0530

    updated carousel

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -34,7 +34,7 @@ function Carousel({images}) {
           <ArrowForwardIosIcon style={{ fontSize: 30 }} />
         </div>
       </div>
-      <br/><br/><br/>
+      <br/><br/>
     </div>
   );
 }
